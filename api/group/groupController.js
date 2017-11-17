7fe6ea1309dcf15c85d872c82e03a561e5c0878f
Danny Le Kim <dannylekim@gmail.com>
2017-11-16 23:16:18 -0500

    named Functions

--- api/group/groupController.js
+++ api/group/groupController.js
@@ -6,9 +6,8 @@ const mongoose = require("mongoose"),
   user = mongoose.model("User"),
   auth = require("../../config/globalFunctions");
 
-
-exports.getGroup = function(req, res) {
-  group.find({ _id: req.params.groupId }, function(err, group) {
+exports.getGroup = function findGroup(req, res) {
+  group.find({ _id: req.params.groupId }, function sendResponse(err, group) {
     if (err) {
       res.send(err);
     } else {
@@ -17,11 +16,11 @@ exports.getGroup = function(req, res) {
   });
 };
 
-exports.createGroup = function(req, res) {
-  req.body.users = [{'userId': req.params.userId, 'taskId': []}]
-  req.body.teamLeader = req.params.userId
+exports.createGroup = function setParametersAndSave(req, res) {
+  req.body.users = [{ userId: req.params.userId, taskId: [] }];
+  req.body.teamLeader = req.params.userId;
   const newGroup = new group(req.body);
-  group.save(function(err, group) {
+  group.save(function sendResponse(err, group) {
     if (err) res.send(err);
     else {
       res.json(group);
@@ -29,61 +28,64 @@ exports.createGroup = function(req, res) {
   });
 };
 
-
-exports.deleteGroup = function(req, res) {
-  group.find({_id: req.params.groupId}, function(err, foundGroup){
-    if(err){
-      res.send(err)
-    }
-    else{
-    var users = []
-    var tasks = []
-    for (user of foundGroup.users){
-      users.push(user.userId)
-      tasks.push(user.taskId)
-    }
-    user.find({_id: {$in: users}}, function(err, foundUsers){
-      for(groupUser in foundUsers){
-        for(let index = 0; i < groupUser.groups.length; i++){
-          if(req.params.id === groupUser.groups[index].groupId){
-            groupUser.groups.splice(i, 1)
-            groupUser.save(function(err, updatedGroupUser){
-              if(err) {
-                res.send(err)
-                return
-              }
-              else{
-                break;
-              }
-            })
+exports.deleteGroup = function findGroup(req, res) {
+  group.find({ _id: req.params.groupId }, function pullUsersAndTasks(
+    err,
+    foundGroup
+  ) {
+    if (err) {
+      res.send(err);
+    } else {
+      var users = [];
+      var tasks = [];
+      for (user of foundGroup.users) {
+        users.push(user.userId);
+        tasks.push(user.taskId);
+      }
+      user.find({ _id: { $in: users } }, function removeGroupFromEachUser(
+        err,
+        foundUsers
+      ) {
+        for (groupUser in foundUsers) {
+          for (let index = 0; i < groupUser.groups.length; i++) {
+            if (req.params.id === groupUser.groups[index].groupId) {
+              groupUser.groups.splice(i, 1);
+              groupUser.save(function updatedUsers(err, updatedGroupUser) {
+                if (err) {
+                  res.send(err);
+                  return;
+                }
+              });
+              break;
+            }
           }
         }
-      }
-    })
-    task.remove({_id: {$in: tasks}}, function(err, tasks){
-      if(err){
-        res.send(err)
-        return;
-      } 
-    })
-      group.remove({_id: req.params.groupId}, function(err, removedGroup){
-        if(err) res.send(err)
-        else{
-          res.json({message:"Group has successfully been removed"})
+      });
+      task.remove({ _id: { $in: tasks } }, function errorHandle(err, tasks) {
+        if (err) {
+          res.send(err);
+          return;
         }
-      })
+      });
+      group.remove({ _id: req.params.groupId }, function sendResponse(
+        err,
+        removedGroup
+      ) {
+        if (err) res.send(err);
+        else {
+          res.json({ message: "Group has successfully been removed" });
+        }
+      });
     }
-  })
-}
+  });
+};
 
-
-
-exports.updateGroup = function(req, res) {
+exports.updateGroup = function findGroup(req, res) {
   group.findByIdAndUpdate(
     { _id: req.params.id },
     req.body,
     { new: true },
-    function(err, group) {
+    function sendResponse(err, group) {
       if (err) res.send(err);
       else {
         res.json(group);
@@ -94,8 +96,8 @@ exports.updateGroup = function(req, res) {
 
 //============= Admin Functions =================
 
-exports.getAllGroups = function(req, res) {
-  group.find({}, function(err, group) {
+exports.getAllGroups = function findAllGroups(req, res) {
+  group.find({}, function sendResponse(err, group) {
     if (err) res.send(err);
     else {
       res.json(group);
