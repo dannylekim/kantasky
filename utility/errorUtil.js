523812d9cffaf715c49efd9dd735ba79862b0ea7
Danny Le Kim <dannylekim@gmail.com>
2017-12-16 01:31:18 -0500

    commented error util, needs refactoring

--- utility/errorUtil.js
+++ utility/errorUtil.js
@@ -1,41 +1,60 @@
-'use strict'
+"use strict";
 
 const logger = require("./logUtil");
 
-const handleError = function handleError(err, req, res, next) {
+/**
+ *  Error Handling Middleware. Checks if operational, checks if there's a status and sends a response appropriately
+ * logs as well
+ *
+ * @param {any} err Error
+ * @param {any} req Request
+ * @param {any} res Response
+ * @param {any} next Next
+ */
+exports.handleError = (err, req, res, next) => {
   if (!err.isOperational) {
-    logger.log('error', req.method + " " + res.url, '', err)
+    logger.log(
+      "error",
+      "NON-OPERATIONAL ERROR AT: " + req.method + " " + res.url,
+      "",
+      err
+    );
     res.send(err.message);
-    process.exit(1);
   }
   if (err.status) {
-    logger.log('warn', req.method + " " + res.url, err.status, err)
-    res.status(err.status).send(err.message)
-  }
-  else {
-    logger.log('warn', req.method + " " + res.url, '', err)
+    logger.log("warn", req.method + " " + res.url, err.status, err);
+    res.status(err.status).send(err.message);
+  } else {  
+    logger.log("warn", req.method + " " + res.url, "", err);
     res.send(err.message);
   }
 };
 
-const handleUncaughtException = function handleUncaughtException(err) {
+/**
+ *  Unhandled exception. If non operational, exit
+ *
+ * @param {any} err error 
+ */
+exports.handleUncaughtException = err => {
   if (!err.isOperational) {
-    logger.log('error', "UNCAUGHT EXCEPTION", "Non-Operational Error", err)
-    console.log(err);
+    logger.log("error", "UNCAUGHT EXCEPTION", "Non-Operational Error", err);
     process.exit(1);
   }
 
-  logger.log('warn', "UNCAUGHT EXCEPTION", "Operational Error", err)
+  logger.log("warn", "UNCAUGHT EXCEPTION", "Operational Error", err);
   throw err;
 };
 
-const createOperationalError = function createError(message, status) {
-  var error = new Error(message);
+/**
+ * Creates an operational error with the message
+ *
+ * @param {any} message message of the error
+ * @param {any} status http status code
+ * @returns
+ */
+exports.createOperationalError = (message, status) => {
+  let error = new Error(message);
   error.isOperational = true;
-  error.status = status;
+  if (status) error.status = status;
   return error;
 };
-
-exports.createOperationalError = createOperationalError;
-exports.handleError = handleError;
-exports.handleUncaughtException = handleUncaughtException;
