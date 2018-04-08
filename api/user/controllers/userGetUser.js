c2001568f18d757609834352a1e31d0280f255e4
Danny Le Kim <dannylekim@gmail.com>
2018-04-08 17:45:00 -0400

    Fix updateGroup, add searchUser

--- api/user/controllers/userGetUser.js
+++ api/user/controllers/userGetUser.js
@@ -46,3 +46,30 @@ exports.getUser = async (req,res, next) => {
       next(err)
     }
   }
+
+
+  /**
+ * Gets a specific user. Used only for searches, removes their details besides first name, last name and email. Search by email.
+ * 
+ * @param {any} req 
+ * @param {any} res 
+ * @param {any} next 
+ */
+exports.searchUser = async (req,res, next) => { 
+  try {
+    //get user and send
+    const foundUser = await user.findOne({email: req.params.email})
+    if(!foundUser) throw errorHandler.createOperationalError("User does not exist", 403)
+
+    const formattedUser = { 
+      firstName: foundUser.firstName,
+      lastName: foundUser.lastName,
+      email: foundUser.email
+    }
+    res.send(formattedUser)
+
+  }
+  catch(err) { 
+    next(err)
+  }
+}
